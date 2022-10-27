ff2653ca70bdd84a3add23f67f6e8a53b7e83c2f
DalinOurn <ourndalin111@gmail.com>
2022-10-27 15:40:38 +0700

    same instance

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -84,7 +84,7 @@ export default {
 
 
     async favorite(store_id) {
-      const user = await fetch('http://54.199.186.190:3001/auth/me', {
+      const user = await fetch('http://18.181.208.245:3001/auth/me', {
         method: 'GET',
         credentials: 'include',
         headers: {
@@ -95,7 +95,7 @@ export default {
       const userData = await user.json(); 
       
       //add to favorite
-       const addFav = await fetch('http://54.199.186.190:3001/favorite/create', {
+       const addFav = await fetch('http://18.181.208.245:3001/favorite/create', {
         method: 'POST',
         credentials: 'include',
         headers: {
--- src/views/dashboard/AdminLogin.vue
+++ src/views/dashboard/AdminLogin.vue
@@ -15,7 +15,7 @@ export default {
     async onSubmit(e) {
       e.preventDefault()
 
-      const res = await fetch('http://54.199.186.190:3001/auth/adminLogin', {
+      const res = await fetch('http://18.181.208.245:3001/auth/adminLogin', {
         method: 'POST',
         credentials: 'include',
         headers: {
